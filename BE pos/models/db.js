eb3cdab9e1e870068ef5c19aae6a6c1095ae5c34
CarlUlip <carljancellulip0607@gmail.com>
2024-11-29 20:38:20 +0800

    Update db.js

--- BE pos/models/db.js
+++ BE pos/models/db.js
@@ -1,1 +1,14 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
+const sequelize = new Sequelize('pos_system', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false,
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch((err) => console.error('Error connecting to the database:', err));
+
+module.exports = sequelize;
